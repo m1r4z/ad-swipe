d1bbde13b420ee4026a8eaebbb0633ec8fb32f1e
minhajulalamcse <minhajulalam.cse@gmail.com>
2022-02-15 20:16:57 +0600

    new svg icons added

---
+++ src/options/components/svg-components/HeartIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HeartIcon = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 0C8.8174 0 5.76516 1.26428 3.51472 3.51472C1.26428 5.76516 0 8.8174 0 12C0 15.1826 1.26428 18.2349 3.51472 20.4854C5.76516 22.7357 8.8174 24 12 24C15.1826 24 18.2349 22.7357 20.4854 20.4854C22.7357 18.2349 24 15.1826 24 12C24 8.8174 22.7357 5.76516 20.4854 3.51472C18.2349 1.26428 15.1826 0 12 0Z" fill="url(#paint0_linear_love)"></path><path d="M15.7098 6C12.4129 6 12.0004 8.736 12.0004 8.736C12.0004 8.736 11.5894 6 8.29235 6C5.12135 6 4.19735 9.333 4.58435 11.115C5.60435 15.825 12.0004 19.125 12.0004 19.125C12.0004 19.125 18.3978 15.825 19.4178 11.115C19.8033 9.333 18.8778 6 15.7098 6Z" fill="white"></path><defs><linearGradient id="paint0_linear_love" x1="12" y1="0" x2="12" y2="24" gradientUnits="userSpaceOnUse"><stop stop-color="#FF6680"></stop><stop offset="1" stop-color="#E61739"></stop></linearGradient></defs></svg>
+    );
+}
+
+export default HeartIcon;
