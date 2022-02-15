2346a7d367551fb2ed236a46e0c34a6131d9d845
minhajulalamcse <minhajulalam.cse@gmail.com>
2022-02-15 20:09:48 +0600

    new svg icons added

--- src/options/components/SingleAd.js
+++ src/options/components/SingleAd.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const SingleAd = () => {
-    return ('singleAd');
+    return (
+        <div className='single-ad-container'>
+            <div className='ad-heading-container'>
+                <div className='logo-title-container'>
+                    <div className='logo-container'>
+                        <img src="" alt="logo"/>
+                    </div>
+                    <div className='title-container'>
+                        <p className='title'>Findy Global</p>
+                        <p><span></span>Sponsored</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SingleAd;
