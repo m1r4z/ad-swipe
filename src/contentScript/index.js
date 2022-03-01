b5a992e44335dfcd9d483991fd1b2a14f9f8b1b5
minhaj <minhajulalam.cse@gmail.com>
2022-03-02 00:23:49 +0600

    new selector added

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -116,7 +116,8 @@ function showAdFn(from) {
 
 					let targetText = (
 						singlePost.querySelector("a[aria-label='Sponsored']") ??
-						singlePost.querySelector("a[aria-label='label']")
+						singlePost.querySelector("a[aria-label='label']") ??
+						singlePost.querySelector("a.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.gmql0nx0.gpro0wi8.b1v8xokw")
 					)?.innerText;
 
 					var filteredAd = currentAdsArray.filter(
